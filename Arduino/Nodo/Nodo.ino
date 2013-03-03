cbeadc784107b9111d236f34ee0413db8e663422
p.k.tonkes@gmail.com <p.k.tonkes@gmail.com@73350e06-10f7-11df-8bff-b18dc4c943ad>
2013-03-03 19:56:08 +0000

    Alle apparaten nu in de code verbijzonderd tot Devices. Tevens tabblade opgeruimt en code anders verdeeld over de tab's.
    = nog niet volledig getest ! =

--- Arduino/Nodo/Nodo.ino
+++ Arduino/Nodo/Nodo.ino
@@ -5,7 +5,7 @@
 * as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty 
 * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-* You received a copy of the GNU General Public License along with this program in file '_COPYING.TXT'.
+* You received a copy of the GNU General Public License along with this program in file 'COPYING.TXT'.
 *
 * Voor toelichting op de licentievoorwaarden zie    : http://www.gnu.org/licenses
 * Uitgebreide documentatie is te vinden op          : http://www.nodo-domotica.nl
