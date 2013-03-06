e86f58729fee9d002d762ca065c1191ec861bd53
p.k.tonkes@gmail.com <p.k.tonkes@gmail.com@73350e06-10f7-11df-8bff-b18dc4c943ad>
2013-03-06 18:46:55 +0000

    Opschonen SVN

---
+++ Arduino/Nodo/EventScanning.ino
@@ -0,0 +1,146 @@
+#define SIGNAL_TIMEOUT_RF         5000 // na deze tijd in uSec. wordt één RF signaal als beëindigd beschouwd.
+#define SIGNAL_TIMEOUT_IR        10000 // na deze tijd in uSec. wordt één IR signaal als beëindigd beschouwd.
+#define SIGNAL_REPEAT_TIME        1000 // Tijd waarbinnen hetzelfde event niet nogmaals via RF of IR mag binnenkomen. Onderdrukt ongewenste herhalingen van signaal
+
+boolean I2C_EventReceived=false;       // Als deze vlag staat, is er een I2C event binnengekomen.
+struct NodoEventStruct I2C_Event;
+
+boolean ScanEvent(struct NodoEventStruct *Event)
+  {
+  byte Fetched=0;
+
+  // we willen graag een voorziening die er voor zorgt dat events die snel achtereenvolgens worden verzonden
+  // niet leidt tot meerdere events. onderstaand een timer en een opslag voor het laatst ontvangen event.
+  static unsigned long PreviousTime=0L;
+  static unsigned long PreviousHash=0L;
+  
+  // I2C: *************** kijk of er data is binnengekomen op de I2Cbus **********************
+  if(I2C_EventReceived)
+    {
+    Fetched=VALUE_SOURCE_I2C;
+    }
+    
+  // IR: *************** kijk of er data staat op IR en genereer een event als er een code ontvangen is **********************
+  else if((*portInputRegister(IRport)&IRbit)==0)// Kijk of er iets op de poort binnenkomt. (Pin=LAAG als signaal in de ether).
+    {
+    if(FetchSignal(PIN_IR_RX_DATA,LOW,SIGNAL_TIMEOUT_IR))// Als het een duidelijk IR signaal was
+      Fetched=VALUE_SOURCE_IR;
+    }
+
+  // RF: *************** kijk of er data start op RF en genereer een event als er een code ontvangen is **********************
+  else if((*portInputRegister(RFport)&RFbit)==RFbit)// Kijk if er iets op de RF poort binnenkomt. (Pin=HOOG als signaal in de ether).
+    {
+    if(FetchSignal(PIN_RF_RX_DATA,HIGH,SIGNAL_TIMEOUT_RF))// Als het een duidelijk RF signaal was
+      Fetched=VALUE_SOURCE_RF;
+    }
+
+  if(Fetched)
+    {
+    if(AnalyzeRawSignal(Event))// als AnalyzeRawSignal een event heeft opgeleverd dan is het struct Event gevuld.
+      {
+      // Reset de timers nadat er een event is binnegekomen. Dit om later voorafgaand aan het zenden, indien nodig, een
+      // korte pauze te nemen zodat de andere Nodo weer gereed staat voor ontvangst.
+//???      DelayTransmission(Fetched,true);
+
+//      // Herhaling onderdrukken.
+//      if(PreviousTime>millis())
+//        {
+//        WaitFree(100);
+//        return false;
+//        }
+//      PreviousTime=millis()+100;
+
+      // Sommige signaal typen kunnen door de zender herhaald worden verzonden.
+      // Indien signaal kort geleden ook ontvangen, dan herhaling gebruiken als checksum. Daarna verdere herhalingen onderdrukken.
+      if(RawSignal.Repeats)
+        {
+        unsigned long Hash=(unsigned long)(Event->Command<<24) || (unsigned long)(Event->Par1<<16) || (unsigned long)(Event->Par2&0xffff);
+        
+        if(Hash!=PreviousHash)
+          {
+          PreviousHash=Hash;
+          return false;
+          }
+          
+        if(PreviousTime>(millis()-SIGNAL_REPEAT_TIME))
+          return false;
+        }
+      PreviousTime=millis();
+
+      Event->Port=Fetched;
+      Event->Direction=VALUE_DIRECTION_INPUT;
+
+      // Een event kan een verzoek bevatten om bevestiging. Doe dit dan pas na verwerking.
+      if(Event->Flags & TRANSMISSION_CONFIRM)
+        RequestForConfirm=true;
+
+      #ifdef NODO_MEGA
+      // registreer welke Nodo's op welke poort zitten en actualiseer tabel.
+      // Wordt gebruikt voor SendTo en I2C communicatie op de Mega.
+      // Hiermee kan later automatisch de juiste poort worden geselecteerd met de SendTo en kan in
+      // geval van I2C communicatie uitsluitend naar de I2C verbonden Nodo's worden gecommuniceerd.
+      NodoOnline(Event->SourceUnit,Event->Port);
+      #endif
+
+      // Als er een specifieke Nodo is geselecteerd, dan moeten andere Nodo's worden gelocked.
+      // Hierdoor is het mogelijk dat een master en een slave Nodo tijdelijk exclusief gebruik kunnen maken van de bandbreedte
+      // zodat de communicatie niet wordt verstoord.  
+      Transmission_SelectedUnit = Event->DestinationUnit;
+     
+      // Als het Nodo event voor deze unit bestemd is, dan klaar. Zo niet, dan terugkeren met een false
+      // zodat er geen verdere verwerking plaatsvindt.
+      if(Event->DestinationUnit==0 || Event->DestinationUnit==Settings.Unit)
+        return true;
+      }
+    }
+  return false;
+  }
+
+
+#ifdef NODO_MEGA
+boolean ScanAlarm(struct NodoEventStruct *Event)
+  {
+  unsigned long Mask;
+  
+  for(byte x=0;x<ALARM_MAX;x++)
+    {
+    if((Settings.Alarm[x]>>20)&1) // Als alarm enabled is, dan ingestelde alarmtijd vergelijke met de echte tijd.
+      {
+      // stel een vergelijkingswaarde op
+      unsigned long Cmp=Time.Minutes%10 | (unsigned long)(Time.Minutes/10)<<4 | (unsigned long)(Time.Hour%10)<<8 | (unsigned long)(Time.Hour/10)<<12 | (unsigned long)Time.Day<<16 | 1UL<<20;
+
+      // In het ingestelde alarm kunnen zich wildcards bevinden. Maskeer de posities met 0xF wildcard nibble        
+      for(byte y=0;y<8;y++)// loop de acht nibbles van de 32-bit Par2 langs
+        {          
+        if(((Settings.Alarm[x]>>(y*4))&0xf) == 0xf) // als in nibble y een wildcard waarde 0xf staat
+          {
+          Mask=0xffffffff  ^ (0xFUL <<(y*4)); // Mask maken om de nibble positie y te wissen.
+          Cmp&=Mask;                          // Maak nibble leeg
+          Cmp|=(0xFUL<<(y*4));                    // vul met wildcard waarde 0xf
+          }
+        }
+      
+     //Serial.print(F("*** debug: Alarm (bron)="));Serial.print(Settings.Alarm[x],HEX);Serial.print(F(", Cmp="));Serial.println(Cmp,HEX); //??? Debug
+     if(Settings.Alarm[x]==Cmp) // Als ingestelde alarmtijd overeen komt met huidige tijd.
+       {
+       // Serial.print(F("*** Time.Minutes="));Serial.print(Time.Minutes);Serial.print(F(", Prev="));Serial.println(AlarmPrevious[x]); //??? Debug
+       if(AlarmPrevious[x]!=Time.Minutes) // Als alarm niet eerder is afgegaan
+         {
+         AlarmPrevious[x]=Time.Minutes;
+
+         ClearEvent(Event);
+         Event->Direction=VALUE_DIRECTION_INPUT;
+         Event->Port=VALUE_SOURCE_CLOCK;
+         Event->Command=CMD_ALARM;
+         Event->Par1=x+1;
+         return true;
+         }
+       }
+     else
+       AlarmPrevious[x]=0xFF;
+     }
+   }
+ return false; 
+ }
+#endif
+
---
+++ Arduino/Nodo/Nodo.ino
@@ -0,0 +1,108 @@
+/****************************************************************************************************************************\
+* Arduino project "Nodo" © Copyright 2013 Paul Tonkes 
+* 
+* This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License 
+* as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+* This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty 
+* of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+* You received a copy of the GNU General Public License along with this program in file 'COPYING.TXT'.
+*
+* Voor toelichting op de licentievoorwaarden zie    : http://www.gnu.org/licenses
+* Uitgebreide documentatie is te vinden op          : http://www.nodo-domotica.nl
+* bugs kunnen worden gelogd op                      : https://code.google.com/p/arduino-nodo/
+* Compiler voor deze programmacode te downloaden op : http://arduino.cc
+* Voor vragen of suggesties, mail naar              : p.k.tonkes@gmail.com
+* Compiler                                          : Arduino Compiler met minimaal versie 1.0.1
+* Libraties                                         : EthernetNodo library vervangt de standaard Ethernet library!
+* Default settings voor verbinding met de WebApp    : HTTPHost www.nodo-domotica.nl/webapp/nodo.php
+*                                                     PortOutput 80
+*                                                     InputPort 6636
+*                                                     Password <gelijk aan in WebApp opgegeven>
+*                                                     Output HTTP,On
+*
+\*************************************************************************************************************************/
+
+
+                                                                     
+                                                                     
+                                                                     
+                                             
+//****************************** Door gebruiker in te stellen: ***************************************************************
+// De code kan worden gecompileerd als een Nodo-Small voor de Arduino met een ATMega328 processor of een Nodo-Mega met een ATMega1280/2560.
+// stel allereerst het juiste board-type en COM-poort in op de Arduino compiler.
+// en pas eventueel de vier include statements aan (zie onder)
+//#include <EthernetNodo.h>
+//#include <SPI.h>
+//#include <Arduino.h>
+//#include <SD.h>
+
+// Stel hieronder in welk Nodo unit nummer u gaat compileren. De set instellingen moet dan wel hieronder beschreven staan!
+// als je eenmalig per unit nummer de definities vastlegt, dan hoef je daarna bij compileren slechts het juiste unit nummer te kiezen.
+
+#define UNIT_NODO          15
+
+// Let op, dit volgende blokje moet u handmatig instellen afhankelijk van het Arduino type!!!!!!!!!!!!
+// Vanwege een bug in de Arduino compiler moeten enkele regels handmatig worden aangepast!
+// Voor een arduino anders dan een Mega1280 of Mega2560 de onderstaande vier regels vooraf laten gaan door twee slashes //
+// Anders worden de niet gebruikte libraries toch meegecompileerd en zal deze niet in een ATMega328 passen.
+
+
+// Geef onderstaand aan welke devices worden gebruikt in deze EN andere Nodo's die ondeling met elkaar communiceren.
+// Deze definities zijn noodzakelijk om de Nodo's onderling goed te kunnen laten communiceren.
+// Onderstaande regels zijn dus niet de feitelijke programmacode die de I/O van het device verzorgen.
+#define DEVICE_01 // Kaku            : Klik-Aan-Klik-Uit / HomeEasy protocol ontvangst
+#define DEVICE_02 // SendKaku        : Klik-Aan-Klik-Uit / HomeEasy protocol verzenden 
+#define DEVICE_03 // NewKAKU         : Klik-Aan-Klik-Uit ontvangst van signalen met automatische codering. Tevens bekend als Intertechno.
+#define DEVICE_04 // SendNewKAKU     : Klik-Aan-Klik-Uit ontvangst van signalen met automatische codering. Tevens bekend als Intertechno.
+#define DEVICE_05 // TempRead        : Temperatuursensor Dallas DS18B20. (Let op; -B- variant, dus niet DS1820)
+
+// hieronder kunt u de instellingen per unit nummer vastleggen. Bij compileren wordt a.d.h.v. het UNIT_NODO nummer de juiste set gekozen.
+// het is handig om per gebruikte Nodo eenmalig een blokje met definities te maken. 
+// dit blokje hoeft daarna alleen gewijzigd te worden als er een wijziging in die Nodo nodig is.
+// Als startpunt hebben we twee Nodo's als voorbeeld gedefinieerd: een Mega, die unit nummer 1 krijgt, en een Small, die nummer 15 ontvangt.
+// ----------------------------------------------------------------------------------------------------------------------------------------
+// Definities voor Nodo unit nummer 1 
+// Hardware    : Arduino Mega 2560, Ethernetkaart
+// Toepassing  :
+#if (UNIT_NODO == 1)
+  #define NODO_MEGA                    1
+  #define ETHERNET                     0 
+  
+  // Devices in gebruik op deze Nodo:
+  #define DEVICE_CORE_01 // Kaku            : Klik-Aan-Klik-Uit / HomeEasy protocol ontvangst  
+  #define DEVICE_CORE_02 // SendKaku        : Klik-Aan-Klik-Uit / HomeEasy protocol verzenden 
+  #define DEVICE_CORE_03 // NewKAKU         : Klik-Aan-Klik-Uit ontvangst van signalen met automatische codering. Tevens bekend als Intertechno.
+  #define DEVICE_CORE_04 // SendNewKAKU     : Klik-Aan-Klik-Uit ontvangst van signalen met automatische codering. Tevens bekend als Intertechno.
+
+#endif 
+// ----------------------------------------------------------------------------------------------------------------------------------------
+
+
+// ----------------------------------------------------------------------------------------------------------------------------------------
+// Definities voor Nodo unit nummer 15
+// Hardware    : Arduino Duemilanove
+// Toepassing  :
+#if (UNIT_NODO == 15)
+
+  // Devices in gebruik op deze Nodo:
+  #define DEVICE_CORE_05 // TempRead        : Temperatuursensor Dallas DS18B20. (Let op; -B- variant, dus niet DS1820)
+
+#endif 
+// ----------------------------------------------------------------------------------------------------------------------------------------
+
+
+// hieronder volgen nog enkele instellingen die u wilt laten gelden voor alle Nodo unit nummers
+#define NODO_HOME                    0     // Home adres van Nodo's die tot één groep behoren (??? Nog niet operationeel!)
+#define PULSE_DEBOUNCE_TIME         10     // pulsen kleiner dan deze waarde worden niet geteld. Bedoeld on verstoringen a.g.v. ruis of dender te voorkomen
+#define PULSE_TRANSITION       FALLING     // FALLING of RISING: Geeft aan op welke flank de PulseCounter start start met tellen. Default FALLING
+#define I2C_START_ADDRESS            1     // Alle Nodo's op de I2C bus hebben een uniek adres dat start vanaf dit nummer. Er zijn max. 32 Nodo's. Let op verlap met andere devices. RTC zit op adres 104.
+#define NODO_30_COMPATIBLE        true     // t.b.v. compatibiliteit met vorige Nodo versie: NewKAKU HEX waarden en UserEvents
+#define BAUD                     19200     // Baudrate voor seriële communicatie.
+#define PASSWORD_MAX_RETRY           5     // aantal keren dat een gebruiker een foutief wachtwoord mag ingeven alvorens tijdslot in werking treedt
+#define PASSWORD_TIMEOUT           300     // aantal seconden dat het terminal venster is geblokkeerd na foutive wachtwoord
+#define TERMINAL_TIMEOUT           600     // Aantal seconden dat, na de laatst ontvangen regel, de terminalverbinding open mag staan.
+#define SMALL_DEBUG_INFO         false     // Laat op serial van een Small debug info zien als er events worden verzonden of ontvangen.
+
+//****************************** Einde gedeelte door gebruiker in te stellen ************************************************
+
+
