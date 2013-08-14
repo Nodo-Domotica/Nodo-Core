fff76045c0d0b7510eaafd4b3116613ff3296c3b
j.h.man@hccnet.nl <j.h.man@hccnet.nl@73350e06-10f7-11df-8bff-b18dc4c943ad>
2013-08-14 09:56:33 +0000

    B555

--- Arduino/Nodo/Devices/Device_008.c
+++ Arduino/Nodo/Devices/Device_008.c
@@ -1,4 +1,3 @@
-
 //#######################################################################################################
 //##################################### Device-08 AlectoV1  #############################################
 //#######################################################################################################
@@ -8,10 +7,10 @@
  * 
  * Auteur             : Nodo-team (Martinus van den Broek) www.nodo-domotica.nl
  * Support            : www.nodo-domotica.nl
- * Datum              : Mrt.2013
- * Versie             : 1.0
+ * Datum              : 12 Aug 2013
+ * Versie             : 1.1
  * Nodo productnummer : n.v.t. meegeleverd met Nodo code.
- * Compatibiliteit    : Vanaf Nodo build nummer 508
+ * Compatibiliteit    : Vanaf Nodo build nummer 555
  * Syntax             : "AlectoV1 <Par1:Sensor ID>, <Par2:Basis Variabele>"
  *********************************************************************************************
  * Technische informatie:
@@ -55,11 +54,17 @@
  *   F = Checksum
  \*********************************************************************************************/
  
-#ifdef DEVICE_008
 #define DEVICE_ID 8
 #define DEVICE_NAME "AlectoV1"
 
 #define WS3500_PULSECOUNT 74
+
+byte ProtocolAlectoCheckID(byte checkID);
+uint8_t ProtocolAlectoCRC8( uint8_t *addr, uint8_t len);
+
+byte ProtocolAlectoValidID[5];
+byte ProtocolAlectoVar[5];
+unsigned int ProtocolAlectoRainBase=0;
 
 boolean Device_008(byte function, struct NodoEventStruct *event, char *string)
 {
@@ -67,7 +72,7 @@ boolean Device_008(byte function, struct NodoEventStruct *event, char *string)
 
   switch(function)
   {
-#ifdef DEVICE_CORE_008
+#ifdef DEVICE_008_CORE
   case DEVICE_RAWSIGNAL_IN:
     {
       if (RawSignal.Number != WS3500_PULSECOUNT) return false;
@@ -136,6 +141,7 @@ boolean Device_008(byte function, struct NodoEventStruct *event, char *string)
       event->Par2=basevar;
       event->SourceUnit    = 0;                     // Komt niet van een Nodo unit af, dus unit op nul zetten
       event->Port          = VALUE_SOURCE_RF;
+      event->Type          = NODO_TYPE_DEVICE_EVENT;
 
       if (basevar == 0) return true;
 
@@ -199,13 +205,14 @@ boolean Device_008(byte function, struct NodoEventStruct *event, char *string)
           {
           ProtocolAlectoValidID[x] = event->Par1;
           ProtocolAlectoVar[x] = event->Par2;
+          success = true;
           break;
           }
         }
       }
     break;
     }
-#endif // DEVICE_CORE_008
+#endif // DEVICE_008_CORE
 
 #if NODO_MEGA
   case DEVICE_MMI_IN:
@@ -218,7 +225,10 @@ boolean Device_008(byte function, struct NodoEventStruct *event, char *string)
       if(strcasecmp(TempStr,DEVICE_NAME)==0)
         {
         if(event->Par1>0 && event->Par1<255 && event->Par2>0 && event->Par2<=USER_VARIABLES_MAX)
-          success=true;
+          {
+            event->Type = NODO_TYPE_DEVICE_COMMAND;
+            success=true;
+          }
         }
       }
       free(TempStr);
@@ -238,10 +248,6 @@ boolean Device_008(byte function, struct NodoEventStruct *event, char *string)
   }      
   return success;
 }
-
-byte ProtocolAlectoValidID[5];
-byte ProtocolAlectoVar[5];
-unsigned int ProtocolAlectoRainBase=0;
 
 /*********************************************************************************************\
  * Calculates CRC-8 checksum
@@ -273,8 +279,4 @@ byte ProtocolAlectoCheckID(byte checkID)
   for (byte x=0; x<5; x++) if (ProtocolAlectoValidID[x] == checkID) return ProtocolAlectoVar[x];
   return 0;
 }
-
-
-#endif //DEVICE_08
-
 
