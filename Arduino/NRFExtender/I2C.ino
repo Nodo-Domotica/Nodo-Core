8214d5b3029d7662dc347e2c48a53cbc4c95813d
mjvdbr@gmail.com <mjvdbr@gmail.com@73350e06-10f7-11df-8bff-b18dc4c943ad>
2014-02-06 13:39:06 +0000

    Prototype Nodo Wireless I2C Extender (NRFExtender)

---
+++ Arduino/NRFExtender/I2C.ino
@@ -0,0 +1,100 @@
+/**********************************************************************************************\
+ * I2C bus receive handler (IRQ) 
+ \*********************************************************************************************/
+
+void ReceiveI2C(int n)
+{
+  I2C_Received=0;
+  byte b;
+
+  while(WireNodo.available()) // Store received bytes into I2C buffer
+  {
+    b=WireNodo.read();
+    I2C_ReceiveBuffer[I2C_Received++]=b; 
+  }
+}
+
+/**********************************************************************************************\
+ * Send event on I2C bus. 
+ \*********************************************************************************************/
+void SendI2C(struct NodoEventStruct *EventBlock)
+{  
+  byte x;
+  byte *B=(byte*)EventBlock;  
+
+  Checksum(EventBlock);// calculate checksum: crc-8
+
+  for(int y=1;y<=UNIT_MAX;y++)
+  {            
+    if((EventBlock->Command==EVENT_BOOT && EventBlock->Type == NODO_TYPE_EVENT) || (NodoOnline(y,0)==VALUE_SOURCE_I2C))
+    {
+      Serial.print("Send to I2C address:");
+      Serial.println((int)y);
+      WireNodo.beginTransmission(I2C_START_ADDRESS+y-1);
+      for(x=0;x<sizeof(struct NodoEventStruct);x++)
+        WireNodo.write(*(B+x));
+      WireNodo.endTransmission(false); // verzend de data, sluit af maar geef de bus NIET vrij
+    }
+  }
+  WireNodo.endTransmission(true); // Geef de bus vrij
+}
+
+/*********************************************************************************************\
+ * I2C Checksum
+ \*********************************************************************************************/
+
+boolean Checksum(NodoEventStruct *event)
+{
+  byte OldChecksum=event->Checksum;
+  byte NewChecksum=NODO_VERSION_MAJOR;  // Verwerk versie in checksum om communicatie tussen verschillende versies te voorkomen
+
+  event->Checksum=0; // anders levert de beginsituatie een andere checksum op
+
+  for(int x=0;x<sizeof(struct NodoEventStruct);x++)
+    NewChecksum^(*((byte*)event+x)); 
+
+  event->Checksum=NewChecksum;
+  return(OldChecksum==NewChecksum);
+}
+
+/*******************************************************************************************************\
+ * Keep track of all Online Nodo units
+ \*******************************************************************************************************/
+byte NodoOnline(byte Unit, byte Port)
+{
+  static byte NodoOnlinePort[UNIT_MAX+1];
+  static boolean FirstTime=true;
+
+  int x;
+
+  // On first call, clear table
+  if(FirstTime)
+  {
+    FirstTime=false;
+    for(x=0;x<=UNIT_MAX;x++)
+      NodoOnlinePort[x]=0;
+  }
+
+  if(Port && Port!=NodoOnlinePort[Unit])
+  {
+    // Update table.
+    if(Port==VALUE_SOURCE_I2C)
+      NodoOnlinePort[Unit]=VALUE_SOURCE_I2C;
+  }    
+  return NodoOnlinePort[Unit];
+}
+
+void PrintNodoOnline(void)
+{
+  Serial.println("Nodo Online:");
+  for(int y=1;y<=UNIT_MAX;y++)
+  {            
+    if((NodoOnline(y,0)==VALUE_SOURCE_I2C))
+    {
+      Serial.print((int)y);
+      Serial.println(" is Online");
+    }
+  }
+}
+
+
---
+++ Arduino/NRFExtender/NRF.ino
@@ -0,0 +1,430 @@
+#define EN_AA       0x01  // Register for Enable Auto Acknowledge configuration register
+#define RF_SETUP    0x06  // Register for Set Data Rate
+#define SETUP_RETR  0x04  // Register for Retry delay and count
+#define STATUS      0x07  // Register for status
+
+#define mirf_ADDR_LEN	5
+#define mirf_CONFIG ((1<<EN_CRC) | (0<<CRCO) )
+
+// NRF Variables
+uint8_t Nrf24_PTX;
+uint8_t Nrf24_channel=1;
+
+byte address[5] = { 1,2,3,4,0 };
+byte NRF_status=0;
+
+void NRF_init(void)
+{
+  pinMode(NRF_CSN_PIN,OUTPUT);
+  pinMode(NRF_CE_PIN,OUTPUT);
+  pinMode(10,OUTPUT); // Needed for SPI !!
+  digitalWrite(NRF_CSN_PIN,HIGH);
+  digitalWrite(NRF_CE_PIN,HIGH);
+  SPI.begin();
+  SPI.setDataMode(SPI_MODE0);
+  SPI.setClockDivider(SPI_2XCLOCK_MASK);
+  Nrf24_init();
+  address[4]=NRF_RECEIVE_ADDRESS;
+  Nrf24_setRADDR((byte *)address);
+  Nrf24_channel = NRF_CHANNEL;
+  Nrf24_config();
+
+  byte rf_setup=0x26; // 250 kbps
+  Nrf24_writeRegister(RF_SETUP, &rf_setup, sizeof(rf_setup) );
+  byte en_aa=0x3F; // Auto-Ack Enabled
+  Nrf24_writeRegister(EN_AA, &en_aa, sizeof(en_aa) );
+  byte setup_retr=0xff;  // delay 4000uS, 15 retries
+  Nrf24_writeRegister(SETUP_RETR, &setup_retr, sizeof(setup_retr) );
+  Nrf24_flushRx();
+}
+
+boolean NRF_receive(struct NodoEventStruct *event)
+{
+  boolean dataready=false;
+  
+  if(!Nrf24_isSending() && Nrf24_dataReady())
+    {
+      byte Payload[NRF_PAYLOAD_SIZE];
+      Nrf24_getData((byte *)Payload);
+      if (Payload[0]==0)
+        {
+          memcpy((byte*)event, (byte*)&Payload+1,sizeof(struct NodoEventStruct));
+          dataready=true;
+        }
+    }
+
+  return dataready;
+}
+
+void NRF_send(struct NodoEventStruct *event)
+{
+
+  byte Payload[NRF_PAYLOAD_SIZE];
+  Payload[0]=0;
+  memcpy((byte*)&Payload+1, (byte*)event,sizeof(struct NodoEventStruct));
+  
+  for(int y=1;y<=NRF_UNIT_MAX;y++)
+    {
+      if(NRFOnline[y]==true)
+        {
+          Serial.print("Send to NRF address: ");
+          Serial.println((int)y);
+          address[4]=y;
+          NRF_status=0;
+          Nrf24_setTADDR((byte *)address);
+          Nrf24_send((byte *)Payload);
+          while(Nrf24_isSending()) {}
+      //    if(NRF_status==46)
+      //      do something...
+        }
+    }
+}
+
+void NRF_CheckOnline()
+{
+  Serial.println("NRF Online Check ");
+  byte Payload[NRF_PAYLOAD_SIZE];
+  for(byte x=0; x < NRF_PAYLOAD_SIZE; x++) Payload[x]=0;
+  Payload[0]=255;
+  for(int y=1;y<=NRF_UNIT_MAX;y++)
+    {
+          address[4]=y;
+          NRF_status=0;
+          Nrf24_setTADDR((byte *)address);
+          Nrf24_send((byte *)Payload);
+          while(Nrf24_isSending()) {}
+
+          if(NRF_status==46)
+            {
+              Serial.print((int)y);
+              Serial.println(" is Online");
+              NRFOnline[y]=true;
+            }
+    }
+}
+
+// ***********************************************************************************************************
+// NRF24 specific code
+// ***********************************************************************************************************
+
+/*
+    Copyright (c) 2007 Stefan Engelke <mbox@stefanengelke.de>
+    Nathan Isburgh <nathan@mrroot.net>
+    An Ardunio port of http://www.tinkerer.eu/AVRLib/nRF24L01
+    Significant changes to remove depencence on interupts and auto ack support: Aaron Shrimpton <aaronds@gmail.com>
+
+    Permission is hereby granted, free of charge, to any person 
+    obtaining a copy of this software and associated documentation 
+    files (the "Software"), to deal in the Software without 
+    restriction, including without limitation the rights to use, copy, 
+    modify, merge, publish, distribute, sublicense, and/or sell copies 
+    of the Software, and to permit persons to whom the Software is 
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be 
+    included in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+    DEALINGS IN THE SOFTWARE.
+*/
+
+/* Memory Map */
+#define CONFIG      0x00
+#define EN_RXADDR   0x02
+#define SETUP_AW    0x03
+#define RF_CH       0x05
+#define STATUS      0x07
+#define OBSERVE_TX  0x08
+#define CD          0x09
+#define RX_ADDR_P0  0x0A
+#define RX_ADDR_P1  0x0B
+//#define RX_ADDR_P2  0x0C
+//#define RX_ADDR_P3  0x0D
+//#define RX_ADDR_P4  0x0E
+//#define RX_ADDR_P5  0x0F
+#define TX_ADDR     0x10
+#define RX_PW_P0    0x11
+#define RX_PW_P1    0x12
+//#define RX_PW_P2    0x13
+//#define RX_PW_P3    0x14
+//#define RX_PW_P4    0x15
+//#define RX_PW_P5    0x16
+#define FIFO_STATUS 0x17
+
+/* Bit Mnemonics */
+#define MASK_RX_DR  6
+#define MASK_TX_DS  5
+#define MASK_MAX_RT 4
+#define EN_CRC      3
+#define CRCO        2
+#define PWR_UP      1
+#define PRIM_RX     0
+//#define ENAA_P5     5
+//#define ENAA_P4     4
+//#define ENAA_P3     3
+//#define ENAA_P2     2
+//#define ENAA_P1     1
+//#define ENAA_P0     0
+//#define ERX_P5      5
+//#define ERX_P4      4
+//#define ERX_P3      3
+//#define ERX_P2      2
+//#define ERX_P1      1
+//#define ERX_P0      0
+#define AW          0
+#define ARD         4
+#define ARC         0
+#define PLL_LOCK    4
+#define RF_DR       3
+#define RF_PWR      1
+#define LNA_HCURR   0        
+#define RX_DR       6
+#define TX_DS       5
+#define MAX_RT      4
+#define RX_P_NO     1
+#define TX_FULL     0
+#define PLOS_CNT    4
+#define ARC_CNT     0
+#define TX_REUSE    6
+#define FIFO_FULL   5
+#define TX_EMPTY    4
+#define RX_FULL     1
+#define RX_EMPTY    0
+
+/* Instruction Mnemonics */
+#define R_REGISTER    0x00
+#define W_REGISTER    0x20
+#define REGISTER_MASK 0x1F
+#define R_RX_PAYLOAD  0x61
+#define W_TX_PAYLOAD  0xA0
+#define FLUSH_TX      0xE1
+#define FLUSH_RX      0xE2
+#define REUSE_TX_PL   0xE3
+#define NOP           0xFF
+
+
+void Nrf24_transferSync(uint8_t *dataout,uint8_t *datain,uint8_t len)
+{
+	uint8_t i;
+	for(i = 0;i < len;i++){
+		datain[i] = SPI.transfer(dataout[i]);
+	}
+}
+
+void Nrf24_transmitSync(uint8_t *dataout,uint8_t len)
+{
+	uint8_t i;
+	for(i = 0;i < len;i++){
+		SPI.transfer(dataout[i]);
+	}
+}
+
+
+void Nrf24_init() 
+// Initializes pins to communicate with the MiRF module, should be called in the early initializing phase at startup.
+{   
+    pinMode(NRF_CE_PIN,OUTPUT);
+    pinMode(NRF_CSN_PIN,OUTPUT);
+
+    Nrf24_ceLow();
+    Nrf24_csnHi();
+
+}
+
+void Nrf24_config() 
+// Sets the important registers in the MiRF module and powers the module in receiving mode. NB: channel and payload must be set now.
+{
+    // Set RF channel
+	Nrf24_configRegister(RF_CH,Nrf24_channel);
+
+    // Set length of incoming payload 
+	Nrf24_configRegister(RX_PW_P0, NRF_PAYLOAD_SIZE);
+	Nrf24_configRegister(RX_PW_P1, NRF_PAYLOAD_SIZE);
+
+    // Start receiver 
+    Nrf24_powerUpRx();
+    Nrf24_flushRx();
+}
+
+void Nrf24_setRADDR(uint8_t * adr) 
+// Sets the receiving address
+{
+	Nrf24_ceLow();
+	Nrf24_writeRegister(RX_ADDR_P1,adr,mirf_ADDR_LEN);
+	Nrf24_ceHi();
+}
+
+void Nrf24_setTADDR(uint8_t * adr)
+// Sets the transmitting address
+{
+	Nrf24_writeRegister(RX_ADDR_P0,adr,mirf_ADDR_LEN);
+	Nrf24_writeRegister(TX_ADDR,adr,mirf_ADDR_LEN);
+}
+
+bool Nrf24_dataReady() 
+// Checks if data is available for reading
+{
+	uint8_t status = Nrf24_getStatus();
+
+    if ( status & (1 << RX_DR) ) return 1;
+    return !Nrf24_rxFifoEmpty();
+}
+
+bool Nrf24_rxFifoEmpty(){
+	uint8_t fifoStatus;
+
+	Nrf24_readRegister(FIFO_STATUS,&fifoStatus,sizeof(fifoStatus));
+	return (fifoStatus & (1 << RX_EMPTY));
+}
+
+
+
+void Nrf24_getData(uint8_t * data) 
+// Reads payload bytes into data array
+{
+    Nrf24_csnLow();                               // Pull down chip select
+    SPI.transfer( R_RX_PAYLOAD );            // Send cmd to read rx payload
+    Nrf24_transferSync(data,data,NRF_PAYLOAD_SIZE); // Read payload
+    Nrf24_csnHi();                               // Pull up chip select
+    Nrf24_configRegister(STATUS,(1<<RX_DR));   // Reset status register
+}
+
+void Nrf24_configRegister(uint8_t reg, uint8_t value)
+// Clocks only one byte into the given MiRF register
+{
+    Nrf24_csnLow();
+    SPI.transfer(W_REGISTER | (REGISTER_MASK & reg));
+    SPI.transfer(value);
+    Nrf24_csnHi();
+}
+
+void Nrf24_readRegister(uint8_t reg, uint8_t * value, uint8_t len)
+// Reads an array of bytes from the given start position in the MiRF registers.
+{
+    Nrf24_csnLow();
+    SPI.transfer(R_REGISTER | (REGISTER_MASK & reg));
+    Nrf24_transferSync(value,value,len);
+    Nrf24_csnHi();
+}
+
+void Nrf24_writeRegister(uint8_t reg, uint8_t * value, uint8_t len) 
+// Writes an array of bytes into inte the MiRF registers.
+{
+    Nrf24_csnLow();
+    SPI.transfer(W_REGISTER | (REGISTER_MASK & reg));
+    Nrf24_transmitSync(value,len);
+    Nrf24_csnHi();
+}
+
+
+void Nrf24_send(uint8_t * value) 
+// Sends a data package to the default address. Be sure to send the correct amount of bytes as configured as payload on the receiver.
+{
+    uint8_t status;
+    status = Nrf24_getStatus();
+
+    while (Nrf24_PTX) {
+	    status = Nrf24_getStatus();
+
+	    if((status & ((1 << TX_DS)  | (1 << MAX_RT)))){
+		    Nrf24_PTX = 0;
+		    break;
+	    }
+    }                  // Wait until last paket is send
+
+    Nrf24_ceLow();
+    Nrf24_powerUpTx();       // Set to transmitter mode , Power up
+    
+    Nrf24_csnLow();                    // Pull down chip select
+    SPI.transfer( FLUSH_TX );     // Write cmd to flush tx fifo
+    Nrf24_csnHi();                    // Pull up chip select
+    
+    Nrf24_csnLow();                    // Pull down chip select
+    SPI.transfer( W_TX_PAYLOAD ); // Write cmd to write payload
+    Nrf24_transmitSync(value,NRF_PAYLOAD_SIZE);   // Write payload
+    Nrf24_csnHi();                    // Pull up chip select
+
+    Nrf24_ceHi();                     // Start transmission
+}
+
+
+bool Nrf24_isSending()
+{
+	uint8_t status;
+	if(Nrf24_PTX){
+		status = Nrf24_getStatus();
+
+		// if sending successful (TX_DS) or max retries exceded (MAX_RT).
+
+		if((status & ((1 << TX_DS)  | (1 << MAX_RT)))){
+			Nrf24_powerUpRx();
+                        NRF_status = status;
+			return false; 
+		}
+
+		return true;
+	}
+	return false;
+}
+
+uint8_t Nrf24_getStatus()
+{
+	uint8_t rv;
+	Nrf24_readRegister(STATUS,&rv,1);
+	return rv;
+}
+
+void Nrf24_powerUpRx()
+{
+	Nrf24_PTX = 0;
+	Nrf24_ceLow();
+	Nrf24_configRegister(CONFIG, mirf_CONFIG | ( (1<<PWR_UP) | (1<<PRIM_RX) ) );
+	Nrf24_ceHi();
+	Nrf24_configRegister(STATUS,(1 << TX_DS) | (1 << MAX_RT)); 
+}
+
+void Nrf24_flushRx()
+{
+    Nrf24_csnLow();
+    SPI.transfer( FLUSH_RX );
+    Nrf24_csnHi();
+}
+
+void Nrf24_powerUpTx()
+{
+	Nrf24_PTX = 1;
+	Nrf24_configRegister(CONFIG, mirf_CONFIG | ( (1<<PWR_UP) | (0<<PRIM_RX) ) );
+}
+
+void Nrf24_ceHi()
+{
+	digitalWrite(NRF_CE_PIN,HIGH);
+}
+
+void Nrf24_ceLow()
+{
+	digitalWrite(NRF_CE_PIN,LOW);
+}
+
+void Nrf24_csnHi()
+{
+	digitalWrite(NRF_CSN_PIN,HIGH);
+}
+
+void Nrf24_csnLow()
+{
+	digitalWrite(NRF_CSN_PIN,LOW);
+}
+
+void Nrf24_powerDown()
+{
+	Nrf24_ceLow();
+	Nrf24_configRegister(CONFIG, mirf_CONFIG );
+}
+
