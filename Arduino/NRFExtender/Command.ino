f2246efb4002dc6ad7557145c715314353a8f896
mjvdbr@gmail.com <mjvdbr@gmail.com@73350e06-10f7-11df-8bff-b18dc4c943ad>
2014-02-09 16:23:54 +0000

    NRFExtender, added ping command several code optimizations

--- Arduino/NRFExtender/Command.ino
+++ Arduino/NRFExtender/Command.ino
@@ -50,19 +50,27 @@ void ExecuteCommand(char *Command, byte Par1, byte Par2)
         Serial.println((int)Settings.Channel);
       }
 
-   if(strcasecmp(Command,"Test")==0)
+   if(strcasecmp(Command,"Ping")==0)
       {
-        NRFPayload.Source=Settings.Address;
-        NRFPayload.Destination=255;
-        NRFPayload.ID=255;
-        NRFPayload.Size=0;
-        Serial.println("Broadcast");
-        byte NRF_tmpaddress[5] = { 255,2,3,4,5 };
-        Nrf24_setTADDR((byte *)NRF_tmpaddress);
-        Nrf24_send((byte *)&NRFPayload);
-        while(Nrf24_isSending()) {}
+        Serial.print("Ping ");
+        Serial.println((int)Par1);
+        unsigned long NRF_roundtrip=millis();
+        NRF_sendpacket(Settings.Address, Par1, NRF_PAYLOAD_PINGREQ, 0);
+        Serial.print("status ");
+        Serial.println((int)NRF_status);
+        Serial.print("roundtrip ");
+        Serial.println(millis()-NRF_roundtrip);
+        byte NRF_retries=0;
+        Nrf24_readRegister(8, &NRF_retries, sizeof(NRF_retries) );
+        Serial.print("retries ");
+        Serial.println((int)(NRF_retries & 0x0f));
       }
 
+   if(strcasecmp(Command,"Test")==0)
+      {
+        NRF_sendpacket(Settings.Address, Par1, Par2, 0);
+      }
+      
    if(strcasecmp(Command,"Freemem")==0)
       {
         Serial.println(FreeMem());
