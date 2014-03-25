72edc0c5c45bb40a87cc991464a1350e10bf8033
mjvdbr@gmail.com <mjvdbr@gmail.com@73350e06-10f7-11df-8bff-b18dc4c943ad>
2014-03-25 16:19:18 +0000

    Plugin updates: Plugin 015: Bugfix, On/Off gaf niet hetzelfde adres Plugin 021: Bugfix, display op Small gaf buffer overflow tijdens boot melding Plugin 030: Bugfix, stabiliteit. Tevens support voor tweede lezer op dezelfde Nodo

--- Arduino/Nodo/Plugins/Plugin_030.c
+++ Arduino/Nodo/Plugins/Plugin_030.c
@@ -7,29 +7,37 @@
  * 
  * Auteur             : Martinus van den Broek / Richard Ten Klooster
  * Support            : www.nodo-domotica.nl
- * Datum              : 25 Jan 2014
- * Versie             : 0.3
+ * Datum              : 25 Mrt 2014
+ * Versie             : 0.6
  * Nodo productnummer : 
  * Compatibiliteit    : Vanaf Nodo build nummer 596
- * Syntax             : "RFIDWG 0,<tag ID>
+ * Syntax             : RFIDWG <reader unit>,<tag ID>
  ***********************************************************************************************
  * Technische beschrijving:
  *
  * Een Wiegand-26 Tag Reader is een device die 125KHz EM4001 compatible tags kan uitlezen. De data wordt via het Wiegand 26 protocol aangeboden
  * Dit betekent dat er twee datalijnen zijn welke korte pulsen genereren voor respectievelijk 0 en 1. Pulsen zijn zeer kort (50uSec) dus IRQ is nodig.
  * We gebruiken de PIN-CHANGE interrupt voorziening van de Atmel.
- * We gebuiken hiervoor twee van de WiredIn aansluitingen van de Nodo
+ * We gebuiken hiervoor twee (of vier) van de WiredIn aansluitingen van de Nodo
  * We maken dus gebruik van een analoge input die we als digitale input met IRQ support gaan gebruiken
  * Op de ATMEGA328P platformen gebruiken we poort A0 en A1
  * Op de ATMEGA2560 platformen gebruiken we poort A8 en A9
  * Elke EM4001 tag levert een unieke code, bestaande uit een 26 bits serienummer
- * Par1 bevat het altijd waarde 0
+ * Par1 bevat het nummer van de reader (1 of 2)
  * Par2 bevat het 26 bits serienummer
  * Dit device genereert een event, zodat actie kan worden ondernomen via de Nodo eventlist indien een bekende tag wordt gebruikt
+ * In de config file dient te worden ingesteld of er 1 of 2 units worden ondersteund
  \*********************************************************************************************/
 
 #define PLUGIN_NAME "RFIDWG"
 #define PLUGIN_ID   30
+
+#if PLUGIN_030_CORE == 1
+  #define PLUGIN_030_BITMASK 0x3
+#else
+  #define PLUGIN_030_BITMASK 0xf
+#endif
+#define PLUGIN_030_WGSIZE 26
 
 #include "Arduino.h"
 #include <avr/interrupt.h>
@@ -39,10 +47,10 @@ inline void pulse_handle_interrupt();
 
 volatile uint8_t *_receivePortRegister;
 
-volatile int bitCount = 0;	// Count the number of bits received.
-unsigned long keyBuffer = 0;	// A 32-bit-long keyBuffer into which the number is stored.
-
-int codeSize = 26; // Assuming it's the Wiegand 26 protocol, there are 26 bits per number.
+volatile byte Plugin_030_bitCount = 0;	           // Count the number of bits received.
+volatile unsigned long Plugin_030_keyBuffer = 0;   // A 32-bit-long keyBuffer into which the number is stored.
+byte Plugin_030_bitCountPrev = 0;                  // to detect noise
+byte Plugin_030_Unit=0;
 
 // In de funktienaam zit het pluginnummer verwerkt zodat deze eenduidig kan worden geidentificeerd en aangeroepen. De Nodo
 boolean Plugin_030(byte function, struct NodoEventStruct *event, char *string)
@@ -59,35 +67,51 @@ boolean Plugin_030(byte function, struct NodoEventStruct *event, char *string)
         #if NODO_MEGA
           SetPinIRQ(A8);
           SetPinIRQ(A9);
+          #if PLUGIN_030_CORE == 2
+            SetPinIRQ(A10);
+            SetPinIRQ(A11);
+          #endif
         #else
           SetPinIRQ(A0);
           SetPinIRQ(A1);
+          #if PLUGIN_030_CORE == 2
+            SetPinIRQ(A2);
+            SetPinIRQ(A3);
+          #endif
         #endif
         break;
       }
 
     case PLUGIN_ONCE_A_SECOND:
       {
-        if (bitCount == codeSize)
-        {
-          bitCount = 0;          // Read in the current key and reset everything so that the interrupts can
+        if ((Plugin_030_bitCount != PLUGIN_030_WGSIZE) && (Plugin_030_bitCount == Plugin_030_bitCountPrev))
+          {
+            // must be noise
+            Plugin_030_bitCount = 0;
+            Plugin_030_keyBuffer = 0;
+          }
 
-          keyBuffer = keyBuffer >> 1;          // Strip leading and trailing parity bits from the keyBuffer
-          keyBuffer &= 0xFFFFFF;
+        if (Plugin_030_bitCount == PLUGIN_030_WGSIZE)
+          {
+            Plugin_030_bitCount = 0;          // Read in the current key and reset everything so that the interrupts can
 
-          struct NodoEventStruct TempEvent;
-          ClearEvent(&TempEvent);
-          TempEvent.SourceUnit = Settings.Unit;
-          TempEvent.Direction  = VALUE_DIRECTION_INPUT;
-          TempEvent.Command    = PLUGIN_ID;
-          TempEvent.Port       = VALUE_SOURCE_WIRED;
-          TempEvent.Par1       = 0;
-          TempEvent.Par2       = keyBuffer;
-          TempEvent.Type       = NODO_TYPE_PLUGIN_EVENT;
-          ProcessEvent(&TempEvent);
+            Plugin_030_keyBuffer = Plugin_030_keyBuffer >> 1;          // Strip leading and trailing parity bits from the keyBuffer
+            Plugin_030_keyBuffer &= 0xFFFFFF;
 
-          keyBuffer = 0;          // Clear the buffer for the next iteration.
-        }
+            struct NodoEventStruct TempEvent;
+            ClearEvent(&TempEvent);
+            TempEvent.SourceUnit = Settings.Unit;
+            TempEvent.Direction  = VALUE_DIRECTION_INPUT;
+            TempEvent.Command    = PLUGIN_ID;
+            TempEvent.Port       = VALUE_SOURCE_WIRED;
+            TempEvent.Par1       = Plugin_030_Unit;
+            TempEvent.Par2       = Plugin_030_keyBuffer;
+            TempEvent.Type       = NODO_TYPE_PLUGIN_EVENT;
+            ProcessEvent(&TempEvent);
+            Plugin_030_keyBuffer = 0;          // Clear the buffer for the next iteration.
+          }
+
+      Plugin_030_bitCountPrev = Plugin_030_bitCount; // store this value for next check, detect noise
       break;
       }
     #endif // CORE
@@ -155,20 +179,33 @@ void SetPinIRQ(byte _receivePin)
 inline void pulse_handle_interrupt()
 /*********************************************************************/
 {
-  byte portstate= *_receivePortRegister & 0x3;
+  byte portstate= *_receivePortRegister & PLUGIN_030_BITMASK;
+
+  if((portstate & 0x3) != 0x3) // first unit!
+      Plugin_030_Unit=1;
+
+  #if PLUGIN_030_CORE == 2
+    if((portstate & 0xC) != 0xC) // second unit!
+      {
+        portstate = portstate >> 2;
+        Plugin_030_Unit=2;
+      }
+  #endif
+
+  portstate = portstate & 0x3;
 
   switch(portstate)
     {
        case 0:			         // Should never happen with Wiegand protocol...
        break;
        case 1: 			         // We've received a 1 bit. (bit 0 = high, bit 1 = low)
-         keyBuffer = keyBuffer << 1;     // Left shift the number (effectively multiplying by 2)
-         keyBuffer += 1;    		 // Add the 1 (not necessary for the zeroes)
-         bitCount++;   			 // Increment the bit count
+         Plugin_030_keyBuffer = Plugin_030_keyBuffer << 1;     // Left shift the number (effectively multiplying by 2)
+         Plugin_030_keyBuffer += 1;    		 // Add the 1 (not necessary for the zeroes)
+         Plugin_030_bitCount++;   			 // Increment the bit count
        break;
        case 2:				 // We've received a 0 bit. (bit 0 = low, bit 1 = high)
-         keyBuffer = keyBuffer << 1;     // Left shift the number (effectively multiplying by 2)
-         bitCount++;       		 // Increment the bit count
+         Plugin_030_keyBuffer = Plugin_030_keyBuffer << 1;     // Left shift the number (effectively multiplying by 2)
+         Plugin_030_bitCount++;       		 // Increment the bit count
          break;
        case 3:				 // Back to "idle state"
        break;
