4747a17864ec610fb7b36611ff72a98df336f108
mjvdbr@gmail.com <mjvdbr@gmail.com@73350e06-10f7-11df-8bff-b18dc4c943ad>
2014-12-15 17:11:36 +0000

    ScanEvent call naar plugin gereduceerd met factor 255.

--- Arduino/Nodo/EventScanning.ino
+++ Arduino/Nodo/EventScanning.ino
@@ -82,10 +82,13 @@ boolean ScanEvent(struct NodoEventStruct *Event)                                
 
 // mvdbro R755 02-12-2014 Experimental support for Sendto within plugins
     #ifdef NODO_BETA_PLUGIN_SENDTO
-    for(byte x=0; x<PLUGIN_MAX; x++)
-      if(Plugin_id[x]==80 || Plugin_id[x]==83 || Plugin_id[x]==33)
-        if(Plugin_ptr[x](PLUGIN_SCAN_EVENT,Event,0))
-          Fetched=VALUE_SOURCE_RF;
+    static byte evtLoopCounter=0;
+    evtLoopCounter++;
+    if (evtLoopCounter == 255)
+      for(byte x=0; x<PLUGIN_MAX; x++)
+        if(Plugin_id[x]==80 || Plugin_id[x]==83 || Plugin_id[x]==33)
+          if(Plugin_ptr[x](PLUGIN_SCAN_EVENT,Event,0))
+            Fetched=VALUE_SOURCE_RF;
     #endif
 // endof mvdbro
 
