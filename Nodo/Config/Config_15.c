89e1628d16e7b54124b5826aff3782f8d5fe06b5
Paul Tonkes <p.k.tonkes@gmail.com>
2015-04-03 17:19:58 +0200

    Build 798
    Small compileer probleem bij uitzetten RawSignal optie

--- Nodo/Config/Config_15.c
+++ Nodo/Config/Config_15.c
@@ -14,10 +14,10 @@
 #define CFG_SOUND                       true                                    // false=geen luidspreker in gebruik.
 #define CFG_WIRED                       true                                    // false=wired voorzieningen uitgeschakeld
 #define CFG_I2C                        false                                    // false=I2C communicatie niet mee compileren (I2C plugins en klok blijvel wel gebruik maken van I2C)
-#define CFG_SLEEP                      false                                    // false=Sleep mode mee compileren.
+#define CFG_SLEEP                       true                                    // false=Sleep mode mee compileren.
 #define CFG_SERIAL                      true                                    // false=Seriele communicatie niet mee compileren. LET OP: hierdoor geen enkele weergave of input via seriele poort meer mogelijk!!! Alleen voor de Nodo-Small 
 #define CFG_RAWSIGNAL                   true                                    // false=Rawsignal niet meecompileren. LET OP: Zowel RF als IR communicatie alsmede diverse plugins gebruiken RawSignal voorzieningen. Alleen voor de Nodo-Small
-#define NODO_PORT_NRF24L01             false                                    // true=Ondersteuning voor tranciever module NRF24L01
+#define NODO_PORT_NRF24L01              true                                    // true=Ondersteuning voor tranciever module NRF24L01
 
 
 // Kaku: Klik-Aan-Klik-Uit
